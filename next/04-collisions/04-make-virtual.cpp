9bd7f7693871d0378299a316c69ee303c62fb643
Egor Suvorov <egor.suvorov@gmail.com>
2021-11-17 14:32:34 +0300

    10-211117: add notes after lecture

---
+++ next/04-collisions/04-make-virtual.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+struct Base {
+    void foo() {
+        std::cout << "Base\n";
+    }
+};
+
+struct Derived : Base {
+    virtual void foo() {
+        std::cout << "Derived\n";
+    }
+    virtual ~Derived() {
+    }
+};
+
+struct SubDerived : Derived {
+    void foo() override {
+        std::cout << "SubDerived\n";
+    }
+};
+
+int main() {
+    SubDerived sd;
+    Derived &d = sd;
+    Base &b = sd;
+
+    sd.foo();  // SubDerived
+    d.foo();  // SubDerived
+    b.foo();  // Base
+
+    sd.Derived::foo();  // Derived
+    sd.Base::foo();  // Base
+}
