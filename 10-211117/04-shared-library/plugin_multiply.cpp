cc6dba59c7ed6c7ef384e2960e0f48c0f846a31c
Egor Suvorov <egor.suvorov@gmail.com>
2021-11-27 22:56:36 +0300

    10-211117/04-shared-library: add example with undefined function

--- 10-211117/04-shared-library/plugin_multiply.cpp
+++ 10-211117/04-shared-library/plugin_multiply.cpp
@@ -2,11 +2,18 @@
 #include <iostream>
 #include "abstract_plugin.h"
 
+void foo();  // Not implemented anywhere
+
 namespace plugin_multiply {
 class plugin_multiply : public plugins_demo::abstract_plugin {
 public:
     plugin_multiply() {
         std::cout << "Constructing plugin_multiply\n";
+        // The following line will cause 'undefined reference' error.
+        // foo();
+        // Windows: linker error during DLL compilation.
+        // Linux: during SO loading in `main.cpp`.
+        // macOS: TODO.
     }
 
     const std::string &name() const override {
