001685186fde8647bb2fe9f4791a8ef51a9d30c1
Egor Suvorov <egor.suvorov@gmail.com>
2021-12-01 14:14:43 +0300

    12-211201: update 04-mi

--- 12-211201/04-mi/02-override-multiple.cpp
+++ 12-211201/04-mi/02-override-multiple.cpp
@@ -1,30 +1,28 @@
 // https://isocpp.org/wiki/faq/multiple-inheritance#mi-delegate-to-sister
 
-struct Base {
+struct Base {  // abstract class
     virtual void foo() = 0;
     virtual void bar() = 0;
 };
 
-struct Derived1 : Base {
+struct Derived1 : Base {  // abstract class
     void foo() override {
     }
 };
 
-struct Derived2 : Base {
+struct Derived2 : Base {  // abstract class
     void bar() override {
     }
 };
 
 struct DerivedX : Derived1, Derived2 {
-    // void foo() override {  // Перезапишет foo() сразу изо всех базовых классов, даже без виртуального наследования.
-                              // Исчезнет неоднозначность.
-                              // См. 19-210302/01-old/02-mi-override.cpp
-    // }
+    void foo() override {  // Overrides foo() from all bases, no ambiguity.
+    }
 };
 
 int main() {
     DerivedX *a = nullptr;
-    a->foo();  // ambiguous: foo() существует в двух экземплярах
-    //a->Derived1::foo();  // без виртуальности
-    //a->Derived1::bar();  // без виртуальности, undefined reference.
+    a->foo();  // ambiguous: there are two foo()
+    //a->Derived1::foo();  // no virtual
+    //a->Derived1::bar();  // no virtual, undefined reference
 }
