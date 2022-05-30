b6ff9fdf52735e86244521a3ce4b70915ca8c22e
Egor Suvorov <egor_suvorov@mail.ru>
2022-05-30 03:11:34 +0300

    28-220523/03-constexpr/03-parse-literal: reference fmtlib

--- 28-220523/03-constexpr/03-parse-literal.cpp
+++ 28-220523/03-constexpr/03-parse-literal.cpp
@@ -23,8 +23,14 @@ static_assert(parse_format("hello%d=%s").second[0] == 'd');
 static_assert(parse_format("hello%d=%s").second[1] == 's');
 // static_assert(parse_format("hello%d=%").second[1] == 's');
 
+// !! NOT FOR EXAM DOWN BELOW !!
+
 // Hard level:
 // https://github.com/hanickadot/compile-time-regular-expressions
+
+// Harder level: https://github.com/fmtlib/fmt
+// Before C++20: at least one non-constexpr argument => function is non-constexpr, so format("%d%d", x) cannot check the format.
+//               Workaround: format(parse_format("%d%d"), x), the `parse_format` function creates an object whose type encodes the format.
 // Since C++20: https://godbolt.org/z/ao4WxK7bv
 
 int main() {}
